573036e81d4641df82f7b1650f6c302af318049a
MAYURNISHANE <mayur.reflectiontp@gmail.com>
2022-09-08 16:11:34 +0530

    simple

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './29-08-2022-App';
+import App from './Componets/29082022/29-08-2022-App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
