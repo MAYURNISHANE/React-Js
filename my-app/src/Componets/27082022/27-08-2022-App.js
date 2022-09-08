d8adf1799f6b5d8559d83a2e3505b931a755be1b
MAYURNISHANE <mayur.reflectiontp@gmail.com>
2022-09-08 16:10:53 +0530

    update text

---
+++ my-app/src/Componets/27082022/27-08-2022-App.js
@@ -0,0 +1,15 @@
+
+import UpdateName from './UpdateName';
+//import './App.css';
+
+function App() {
+  //React Hooks
+  return (
+    <div className="App">
+      <UpdateName/>
+          
+    </div>
+  );
+}
+
+export default App;
